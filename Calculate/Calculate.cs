7831a58710688dc0a5caa882a617d9416f963885
Vitaliy Skoblin <Vitaliy_Skoblin@epam.com>
2017-07-13 09:59:03 +0400

    Расчет времени обхода цикла вынесен в свойство Overhead.

--- Calculate/Calculate.cs
+++ Calculate/Calculate.cs
@@ -6,6 +6,7 @@
     {
         public const int CountOfObject = 2000000;
         private static Stopwatch watch;
+        private static long overhead = -1;
 
         protected static Stopwatch Watch
         {
@@ -20,29 +21,37 @@
             }
         }
 
+        private static long Overhead
+        {
+            get
+            {
+                if (overhead == -1)
+                {
+                    watch.Reset();
+                    watch.Start();
+
+                    for (var i = 0; i < CalculateArrayList.CountOfObject; i++)
+                    {
+                    }
+
+                    watch.Stop();
+
+                    overhead = watch.ElapsedMilliseconds;
+                }
+
+                return overhead;
+            }
+        }
+
         public abstract long GetPerformanceToAdd();
 
         public abstract long GetPerformanceToSearch();
 
         public abstract long GetPerformanceToClear();
 
-        protected static long GetOverhead()
-        {
-            watch.Reset();
-            watch.Start();
-
-            for (var i = 0; i < CalculateArrayList.CountOfObject; i++)
-            {
-            }
-
-            watch.Stop();
-
-            return watch.ElapsedMilliseconds;
-        }
-
         protected static long GetResultWithoutOverhead()
         {
-            return Watch.ElapsedMilliseconds - Calculate.GetOverhead();
+            return Watch.ElapsedMilliseconds - Calculate.Overhead;
         }
     }
 }
